b945e5a4e7841c048fb77ecc8309e64d0c0aa749
frontc <fangchao@lefer.cn>
2020-08-17 14:30:32 +0800

    完善命令行参数.

--- src/main/java/cn/lefer/Main.java
+++ src/main/java/cn/lefer/Main.java
@@ -23,38 +23,45 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //读取配置文件
-//        String configFileName = "/Users/simq/tool/config.json";
+        // 读取配置文件
+        // String configFileName = "/Users/simq/tool/config.json";
+        if (args.length != 0 || args.length != 2) {
+            System.out.println("命令行参数无法识别。请通过-help了解使用方法。");
+            return;
+        }
+        if (args.length == 2 && args[0].equals("-help")) {
+            System.out.println("Api接口数据导入工具。\n\t-config\t配置文件路径。\n\t-help\t查看帮助信息。");
+            return;
+        }
         String configFileName = "config.json";
-        if(args.length>1&&args[0].equals("-config")){
+        if (args.length == 2 && args[0].equals("-config")) {
             configFileName = args[1];
         }
         Config config = getConfig(configFileName);
-        //读取数据文件并转为json
+        // 读取数据文件并转为json
         Stream<String> dataStream = Files.lines(Paths.get(config.getData())).skip(config.getSkip());
         List<String> sourceList = dataStream.collect(Collectors.toList());
-        List<String>  datalist = sourceList.stream().map(s -> toJson(s,config.getSchema())).collect(Collectors.toList());
-        //发起http请求
+        List<String> datalist = sourceList.stream().map(s -> toJson(s, config.getSchema()))
+                .collect(Collectors.toList());
+        // 发起http请求
         HttpClient client = HttpClientBuilder.create().build();
         HttpPost post = new HttpPost(config.getApi());
-        post.setHeader("Authorization",config.getAuth());
-        post.setHeader("Accept","application/json");
-        post.setHeader("Content-type","application/json");
-        for(int i=0;i<datalist.size();i++){
+        post.setHeader("Authorization", config.getAuth());
+        post.setHeader("Accept", "application/json");
+        post.setHeader("Content-type", "application/json");
+        for (int i = 0; i < datalist.size(); i++) {
             post.setEntity(new StringEntity(datalist.get(i)));
             HttpResponse response = client.execute(post);
-            String sb = sourceList.get(i) + "," +
-                    response.getStatusLine().getStatusCode() +
-                    "," +
-                    EntityUtils.toString(response.getEntity());
+            String sb = sourceList.get(i) + "," + response.getStatusLine().getStatusCode() + ","
+                    + EntityUtils.toString(response.getEntity());
             sourceList.set(i, sb);
         }
-        //输出执行结果
+        // 输出执行结果
         List<String> result = new ArrayList<>();
         result.add("\ufeff");
         result.addAll(sourceList);
-        String outputFileName = "result_"+LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8"))+".csv";
-        Files.write(Paths.get(outputFileName),result);
+        String outputFileName = "result_" + LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8")) + ".csv";
+        Files.write(Paths.get(outputFileName), result);
     }
 
     private static Config getConfig(String configFilePathStr) throws IOException {
@@ -63,27 +70,27 @@ public class Main {
         return gson.fromJson(reader, Config.class);
     }
 
-    private static String toJson(String sourceStr,String schema){
+    private static String toJson(String sourceStr, String schema) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("{");
         String[] values = sourceStr.split(",");
         String[] names = schema.split(",");
-        if(values.length!=names.length){
-            throw new RuntimeException("Schema与Data不匹配:"+ Arrays.toString(names) +"-"+ Arrays.toString(values));
+        if (values.length != names.length) {
+            throw new RuntimeException("Schema与Data不匹配:" + Arrays.toString(names) + "-" + Arrays.toString(values));
         }
-        for(int i=0;i<names.length;i++){
+        for (int i = 0; i < names.length; i++) {
             String[] nameAndType = names[i].split("\\|");
             stringBuilder.append("\"");
             stringBuilder.append(nameAndType[0]);
             stringBuilder.append("\":");
-            if(nameAndType.length>1&&nameAndType[1].equals("number")){
+            if (nameAndType.length > 1 && nameAndType[1].equals("number")) {
                 stringBuilder.append(values[i]);
-            }else{
+            } else {
                 stringBuilder.append("\"");
                 stringBuilder.append(values[i]);
                 stringBuilder.append("\"");
             }
-            if(i!=names.length-1){
+            if (i != names.length - 1) {
                 stringBuilder.append(",");
             }
         }
